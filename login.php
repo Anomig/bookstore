8f97ee89b4b5314462f89262fe7a11a51074a13c
Anomig <goyvaerts.naomi@hotmail.com>
2024-11-26 19:01:46 +0100

    .

--- login.php
+++ login.php
@@ -5,8 +5,6 @@ error_reporting(E_ALL);
 session_start(); 
 
 include_once(__DIR__ . "/classes/Db.php");
-var_dump(__DIR__); // Check the current directory
-var_dump(file_exists(__DIR__ . '/classes/Db.php')); // Check if the Db.php file exists at this path
 include_once(__DIR__ . "/classes/Users.php");
 
 if(!empty($_POST)){
